4e9dc96783a78197c1106ea845b36f61cb1875ac
DanielValchev92 <dvalchev92@gmail.com>
2022-02-10 17:15:12 +0200

    node server for the json data

--- Products/app.js
+++ Products/app.js
@@ -1,8 +1,25 @@
+const http = require('http');
+const fs = require('fs');
+const port = 3000;
 
-fetch(".products.json")
-    .then(function (resp) {
-        return resp.json();
+const server = http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'json' })
+    fs.readFile('products.json', function (error, data) {
+        if (error) {
+            res.writeHead(404)
+            res.write('Error: File not found')
+        } else {
+            res.write(data)
+        }
+        res.end();
     })
-    .then(function (data) {
-        console.log(data.name);
-    })
+})
+
+server.listen(port, function (error) {
+    if (error) {
+        console.log('Something went wrong', error);
+    } else {
+        console.log('Server is listening on port ' + port);
+    }
+})
+
